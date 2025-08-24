0c97c9d713b88fe82849f103d3874c420280cff9
Matthieu Labaune <matthieu.labaune@gmail.com>
2025-08-24 17:35:11 +0200

    feat(routes): Add project edit route for complete CRUD operations
    - Add GET /projects/{project}/edit route for project editing
    - Enables full project management workflow
    - Completes RESTful project resource routes

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
     Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
     Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
     Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 
